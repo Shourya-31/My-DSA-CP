2e2248e7c2981a38a60dffad78b933f673283f6c
sho-31 <shourya31veer@gmail.com>
2025-12-19 11:45:14 +0530

    Remove DUplicates using Recursion

--- CP-DSA/DSA/Recursion/Problems/P1.cpp
+++ CP-DSA/DSA/Recursion/Problems/P1.cpp
@@ -1,3 +1,50 @@
 // Tiling Problem: Given a 2 x N board and tiles of size 2 x 1, count the number of ways to tile the board.
-// Time Complexity: O(n)
+// Time Complexity: O(2^N) Exponential
+// Space Complexity: O(N) for the recursion stack
 // DP gives most optimal solution but here we will solve using recursion
+
+// Approach:
+
+// 1. If we place a tile vertically, we are left with a 2 x (n-1) board.
+// And in it will be 2 ways to place remaining tiles. 
+// a.) Place remaining tiles vertically
+// b.) Place remaining tiles horizontally   
+// 2. If we place two tiles horizontally, we are left with a 2 x (n-2) board.
+
+// Therefore, the Clarity must be:
+// What work is being done in each recursive call?
+// In each recursive call, we are reducing the problem size by either 1 or 2
+// To place thee tile either vertically or horizontally
+
+
+// And the Base Case must be:
+// If n == 0 or n == 1, return 1
+// For n == 0, there is one way to tile the board (do nothing)
+// For n == 1, there is one way to tile the board (place one tile vertically)
+
+// Recursive Relation: 
+// T(n) = T(n-1) + T(n-2) => Taking solutions of both the cases
+
+// Solution Code:
+#include <iostream>
+using namespace std;
+
+int tilingProblem(int n) {
+    // Base Case
+    if (n == 0 || n == 1) {
+        return 1;
+    } else {
+        int tp1 = tilingProblem(n - 1); // Place tile vertically
+        int tp2 = tilingProblem(n - 2); // Place tile horizontally
+
+        return tp1 + tp2; // Total ways
+    }
+}
+    
+
+int main() {
+    int n;
+    cin>>n;
+    cout << tilingProblem(n) << endl;
+    return 0;
+}
---
+++ CP-DSA/DSA/Recursion/Problems/P2.cpp
@@ -0,0 +1,51 @@
+// Remove Duplicates in String using Recursion
+// For example, for input string "aabbcc", the output should be "abc"
+
+// We will analyze the problem step by step:
+// 1. What work is being done in each recursive call?
+// In each recursive call, we will check the first character of the string
+// and see if it is already present in the result string formed so far.     
+
+// 2. What is the base case?
+// The base case will be when the input string is empty.
+// In that case, we will return the result string formed so far.
+
+// Recursive Relation:
+// We will make a recursive call by removing the first character of the input string
+
+// To track if the character is already present in the result string
+// We will make a boolean array of size 26 (for lowercase letters) initialized to false
+// When we encounter a character, we will mark its corresponding index in the boolean array as true
+// If we encounter the character again, we will skip it
+
+// map[i] = true indicates that character 'a' + i is already present in the result string, in that case we will skip it
+// else we will include it in the result string and mark map[i] = true
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void removeDuplicates(string str, string ans, int i, int map[26]){
+    if(i == str.length()){
+        cout<<"Ans: "<<ans<<endl;
+        return;
+    }
+
+    char ch = str[i];
+    int mapIdx = (int)(ch - 'a'); // a => 0, b => 1, c => 2 ...
+
+    if (map[mapIdx] == true) { // Duplicate found
+        removeDuplicates(str, ans, i + 1, map); // skip the character
+    } else { // Not a duplicate
+        map[mapIdx] = true;
+        removeDuplicates(str, ans + str[i], i+1, map); // include the character
+    }
+}
+
+int main() {
+    string str;
+    cin>>str;
+    string ans = "";
+    int map[26] = {false}; // Initialize all to false
+    removeDuplicates(str, ans, 0, map);
+    return 0;
+}
