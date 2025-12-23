54a7121bac67924c192318533fb46fb6f9cf4154
sho-31 <shourya31veer@gmail.com>
2025-12-23 21:19:24 +0530

    L4 Completed

--- CP-DSA/DSA/Backtracking/L4permutations.cpp
+++ CP-DSA/DSA/Backtracking/L4permutations.cpp
@@ -10,3 +10,29 @@
 //        f(del str[i], ans + ch) => remove ch from str and add it to ans
 // }
 
+#include <bits/stdc++.h>
+using namespace std;
+
+void permutations(string str, string ans)
+{
+    if (str.size() == 0)
+    {
+        cout << ans << "\n";
+        return;
+    }
+
+    for (int i = 0; i < str.size(); i++)
+    {
+        char ch = str[i];
+        // "abcdefghilu", i = 2 => "ab"+"defghilu" => str.substr(0,i)+str.substr(i+1,str.size()-i-1);
+        string nextStr = str.substr(0, i) + str.substr(i + 1, str.size() - i - 1);
+        permutations(nextStr, ans + ch); // ith char choice to add in permutation
+    }
+}
+
+int main() {
+    string str = "abcde";
+    string ans = "";
+    permutations(str, ans);
+    return 0;
+}
