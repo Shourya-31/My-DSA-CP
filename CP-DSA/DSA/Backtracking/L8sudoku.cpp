1b27ba22b535b3cecc29b697299d5a9eca04d450
sho-31 <shourya31veer@gmail.com>
2025-12-24 01:45:30 +0530

    Logic embeded

---
+++ CP-DSA/DSA/Backtracking/L8sudoku.cpp
@@ -0,0 +1,64 @@
+// Sudoku Solver using Backtracking
+// Rules for Sudoku:
+// 1. Each row must contain the digits from 1 to 9 without repetition.
+// 2. Each column must contain the digits from 1 to 9 without repetition.
+// 3. Each of the nine 3x3 subgrids must contain the digits from 1 to 9 without repetition.
+
+// We need to backtrack the moment we find that a number cannot be placed in any cell.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void printSudoku(int sudoku[9][9]) {
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            cout << sudoku[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+bool sudokuSolver(int sudoku[9][9], int row, int col){
+    // Base Case
+    if(row == 9){
+        printSudoku(sudoku);
+        return true;
+    }
+
+    int nextRow =row, nextCol = col+1;
+    if(col+1 == 9){
+        nextRow = row+1;
+        nextCol = 0;
+    }
+
+    if(sudoku[row][col] != 0){
+        return sudokuSolver(sudoku, nextRow, nextCol);
+    }
+
+    for(int digit=1; digit<=9; digit++){
+        if(isSafe(sudoku, row, col, digit)) {
+            sudoku[row][col] = digit;
+            if(sudokuSolver(sudoku, nextRow, nextCol)){
+                return true;
+            }
+            sudoku[row][col] = 0;
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    int sudoku[9][9] = {
+        {0, 0, 8, 0, 0, 0, 0, 0, 0},
+        {4, 9, 0, 1, 5, 7, 0, 0, 2},
+        {0, 0, 3, 0, 0, 4, 1, 9, 0},
+        {1, 8, 5, 0, 6, 0, 0, 2, 0},
+        {0, 0, 0, 0, 2, 0, 0, 6, 0},
+        {9, 6, 0, 4, 0, 5, 3, 0, 0},
+        {0, 3, 0, 0, 7, 2, 0, 0, 4},
+        {0, 4, 9, 0, 3, 0, 0, 5, 7},
+        {8, 2, 7, 0, 0, 9, 0, 1, 3}};
+
+    return 0;
+}
