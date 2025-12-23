5008905a6ab3310c07ac8d061e450915159396fa
sho-31 <shourya31veer@gmail.com>
2025-12-24 01:10:49 +0530

    Optimization of Grid ways

---
+++ CP-DSA/DSA/Backtracking/L7gridWaysOptimization.cpp
@@ -0,0 +1,4 @@
+// Optimization for Linear Time Complexity
+// Problem: Count the number of ways to reach from the top-left corner to the bottom-right corner of an m x n grid, moving only down or right.
+// Approach: Use combinatorial mathematics to calculate the number of unique paths.
+// So Total Ways = (n-1 + m-1)! / (n-1)! * (m-1)!
