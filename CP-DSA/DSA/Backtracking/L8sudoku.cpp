019e98fc1d55defac88d9bafcc9f7c63bd688be9
sho-31 <shourya31veer@gmail.com>
2025-12-24 01:57:04 +0530

    Sudoku done

--- CP-DSA/DSA/Backtracking/L8sudoku.cpp
+++ CP-DSA/DSA/Backtracking/L8sudoku.cpp
@@ -18,6 +18,35 @@ void printSudoku(int sudoku[9][9]) {
     }
 }
 
+bool isSafe(int sudoku[9][9], int row, int col, int digit){
+    // Check in the row
+    for(int j=0; j<9; j++){
+        if(sudoku[row][j] == digit){
+            return false;
+        }
+    }
+
+    // Check in the column
+    for(int i=0; i<9; i++){
+        if(sudoku[i][col] == digit){
+            return false;
+        }
+    }
+
+    // Check in the 3x3 subgrid
+    int startRow = (row/3)*3;
+    int startCol = (col/3)*3;
+    for(int i=startRow; i<=startRow+2; i++){
+        for(int j=startCol; j<=startCol+2; j++){
+            if(sudoku[i][j] == digit){
+                return false;
+            }
+        }
+
+    }
+    return true;
+}
+
 bool sudokuSolver(int sudoku[9][9], int row, int col){
     // Base Case
     if(row == 9){
@@ -59,6 +88,8 @@ int main()
         {0, 3, 0, 0, 7, 2, 0, 0, 4},
         {0, 4, 9, 0, 3, 0, 0, 5, 7},
         {8, 2, 7, 0, 0, 9, 0, 1, 3}};
+
+    sudokuSolver(sudoku, 0, 0);
 
     return 0;
 }
