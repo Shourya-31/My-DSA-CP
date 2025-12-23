2c1ae7adfbf42eeb466a30271fbf1d6ed64bdee9
sho-31 <shourya31veer@gmail.com>
2025-12-23 20:49:34 +0530

    Find subsets

---
+++ CP-DSA/DSA/Backtracking/L3FindSubsets.cpp
@@ -0,0 +1,5 @@
+// Find & print all subsets of a set of a given string using backtracking
+// Approach: At every character we have two choices either to include it in the current subset or exclude it.
+// So we recurse and backtrack for every character in the string.
+// Making a W shaped pattern.
+
