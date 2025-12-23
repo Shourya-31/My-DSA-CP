3ccf459c963e25e553dfeb52bf1c95bc901e0d8e
sho-31 <shourya31veer@gmail.com>
2025-12-23 20:59:03 +0530

    Find subsets solution

--- CP-DSA/DSA/Backtracking/L3FindSubsets.cpp
+++ CP-DSA/DSA/Backtracking/L3FindSubsets.cpp
@@ -30,3 +30,30 @@
 
 // Base Case: when str is empty print the subset.
 
+#include <bits/stdc++.h>
+using namespace std;
+
+void printSubsets(string str, string subset){
+    if(str.size() == 0){
+        cout<<subset<<"\n";
+        return;
+    }
+
+    char ch = str[0];
+
+    //yes choice
+    printSubsets(str.substr(1,str.size()-1), subset + ch);
+
+    //no choice
+    printSubsets(str.substr(1,str.size()-1), subset);
+}
+
+int main(){
+    string str = "abc";
+    string subset = "";
+
+    printSubsets(str, subset);
+    return 0;
+}
+
+// Sabse pehele YES choices aayengi phir NO choices aayengi.
