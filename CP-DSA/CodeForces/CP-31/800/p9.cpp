4f7e9473c48f207b3600faf89b2f6b769d03f4b7
sho-31 <shourya31veer@gmail.com>
2026-01-05 00:24:24 +0530

    Dopamin hit successfull

---
+++ CP-DSA/CodeForces/CP-31/800/p9.cpp
@@ -0,0 +1,28 @@
+// Solution
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+
+    while(t--){
+        int n;
+        cin>>n;
+
+        int arr[n-1];
+        for(int i=0; i<n-1; i++){
+            cin>>arr[i];
+        }
+
+        int sum = 0;
+
+        for(int i=0; i<n-1; i++){
+            sum = sum+arr[i];
+        }
+
+        int missing = -(sum);
+
+        cout<<missing<<endl;
+    }
+}
