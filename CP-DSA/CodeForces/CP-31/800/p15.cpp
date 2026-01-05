2cc59533b9c36d2b3380d4c1b332f5fb9ec7208f
sho-31 <shourya31veer@gmail.com>
2026-01-05 14:10:11 +0530

    15 in total done. 15th

---
+++ CP-DSA/CodeForces/CP-31/800/p15.cpp
@@ -0,0 +1,41 @@
+// Solution
+#include <bits./stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        long long n;
+        cin>>n;
+        vector<long long> arr(n);
+
+        int odd = 0, even = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+            if (arr[i] % 2 == 0)
+            {
+                even++;
+            }
+            else
+            {
+                odd++;
+            }
+        }
+        
+        if (odd % 2 == 0)
+        {
+            cout << "Yes" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
