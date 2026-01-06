e7b7babc8997df82f44110f74d4da7bffd145a64
sho-31 <shourya31veer@gmail.com>
2026-01-06 21:25:25 +0530

    p22 doneee

--- CP-DSA/CodeForces/CP-31/800/p22.cpp
+++ CP-DSA/CodeForces/CP-31/800/p22.cpp
@@ -1,2 +1,27 @@
 // Solution
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        long long n, k;
+        cin >> n >> k;
+        
+        // we took y = 0 or y = 1
+        if (n % 2 == 0 || (n - k) % 2 == 0)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
