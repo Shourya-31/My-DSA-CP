847a5928b257694ce282f74e7bfcb4a35fb7c6c0
sho-31 <shourya31veer@gmail.com>
2026-01-08 23:22:01 +0530

    p4/900 done

--- CP-DSA/CodeForces/CP-31/900/p4.cpp
+++ CP-DSA/CodeForces/CP-31/900/p4.cpp
@@ -2,26 +2,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int t;
-    cin >> t;                      // number of test cases
+    cin >> t; // number of test cases
 
-    while (t--) {
+    while (t--)
+    {
         long long a, b;
         int n;
-        cin >> a >> b >> n;         // a = max timer value
-                                    // b = initial timer
-                                    // n = number of tools
+        cin >> a >> b >> n; // a = max timer value
+                            // b = initial timer
+                            // n = number of tools
 
-        long long ans = b;          // start with initial time 'b'
-                                    // this is the time bomb survives without tools
+        long long ans = b; // start with initial time 'b'
+        // this is the time bomb survives without tools
 
-        for (int i = 0; i < n; i++) {
+        long long x[n];
+        for (int i = 0; i < n; i++)
+        {
             long long x;
-            cin >> x;               // x = timer increase of current tool
+            cin >> x; // x = timer increase of current tool
 
             /*
              Each tool can increase the timer by x.
@@ -34,12 +38,12 @@ int main() {
              real contribution of this tool = min(x, a - 1)
             */
 
-            ans += min(x, a - 1);   // add maximum useful seconds from this tool
-//             - The timer never actually stays at value 'a'
-//             - The maximum value you can start a new second with is (a - 1)
+            ans += min(x, a - 1); // add maximum useful seconds from this tool
+                                  //             - The timer never actually stays at value 'a'
+                                  //             - The maximum value you can start a new second with is (a - 1)
         }
 
-        cout << ans << '\n';        // total maximum time before explosion
+        cout << ans << '\n'; // total maximum time before explosion
     }
 
     return 0;
