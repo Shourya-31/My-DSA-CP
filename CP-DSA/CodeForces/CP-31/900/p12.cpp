7c8a5e4a4bb8fa895c2dfadd989787fde695581d
sho-31 <shourya31veer@gmail.com>
2026-01-10 14:57:26 +0530

    p12/900 done

--- CP-DSA/CodeForces/CP-31/900/p12.cpp
+++ CP-DSA/CodeForces/CP-31/900/p12.cpp
@@ -1,1 +1,34 @@
 // Solution
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+
+    while(t--){
+        int n;
+        cin>>n;
+
+        long long a[n];
+        for(int i=0; i<n; i++){
+            cin>>a[i];
+           if(a[i] == 1){
+                a[i] = 2;
+            }
+        }
+
+        for(int i = 0; i < n - 1; i++){
+            if(a[i+1] % a[i] == 0){
+                a[i+1] = a[i+1]+1;
+            }
+            
+            
+        }
+
+        for(int i=0; i<n; i++){
+            cout<<a[i]<<" ";
+        }
+        cout<<endl;
+    }
+}
