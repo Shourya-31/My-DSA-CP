bb7c66d95afd6f511ecdbf3ebb5d557d126e9ff6
sho-31 <shourya31veer@gmail.com>
2026-01-13 13:21:23 +0530

    p17 refined

--- CP-DSA/CodeForces/CP-31/900/p17.cpp
+++ CP-DSA/CodeForces/CP-31/900/p17.cpp
@@ -1,50 +1,48 @@
 // Solution
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main()
+{
     int t;
-    cin>>t;
+    cin >> t;
 
-    while(t--){
+    while (t--)
+    {
         long long n;
-        cin>>n;
+        cin >> n;
 
         vector<long long> arr(n);
-        bool same = false;
-        for(int i=0; i<n; i++){
-            cin>>arr[i];
-        }
-
-        for(int i=0; i<n-1; i++){
-            if(arr[i] == arr[i+1]){
-                same = true;
-            }else{
-                same = false;
-            }
-        }
-
-        if(same){
-            cout<<0<<endl;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
         }
 
         int ops = 0;
+        sort(arr.begin(), arr.end());
 
-        unordered_map<long long,long long> freq;
-        long long max_freq = 0;
+        unordered_map<long long, long long> freq;
+        long long max_freq = 1, curr = 1;
 
-        for(long long x : arr){
-            freq[x]++;
-            max_freq = max(max_freq, freq[x]);
+        for (int i = 1; i < n; i++)
+        {
+            if (arr[i] == arr[i - 1])
+                curr++;
+            else
+                curr = 1;
+
+            max_freq = max(max_freq, curr);
         }
 
-        while(max_freq < n){
+
+        while (max_freq < n)
+        {
             ops++;
-        long long use = min(max_freq,n - max_freq);
+            long long use = min(max_freq, n - max_freq);
             ops += use;
             max_freq += use;
         }
-        cout<<ops<<endl;
+        cout << ops << endl;
     }
-    return  0;
+    return 0;
 }
