0c08801a3efdf4ac05ddb671a3e525093e7b90f3
sho-31 <shourya31veer@gmail.com>
2026-01-22 06:53:56 +0530

    p 25 done

---
+++ CP-DSA/CodeForces/CP-31/900/p25.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        long long n, m, i, j;
+        cin >> n >> m >> i >> j;
+
+        // placing yo-yos at opposite corners, (1,1) and (n,m)
+        cout<<1<<" "<<1<<" "<<n<<" "<<m<<"\n";
+    }
+}
