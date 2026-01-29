1f55a2b50bbe78690013fdb6307adadca96d22c8
sho-31 <shourya31veer@gmail.com>
2026-01-29 21:35:04 +0530

    A and B done

--- CP-DSA/CodeForces/Contests/Div2R1077/A.cpp
+++ CP-DSA/CodeForces/Contests/Div2R1077/A.cpp
@@ -5,6 +5,7 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+    
     int t;
     cin >> t;
     while (t--)
--- CP-DSA/CodeForces/Contests/Div2R1077/B.cpp
+++ CP-DSA/CodeForces/Contests/Div2R1077/B.cpp
@@ -1,34 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
 
-int main()
-{
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
     int t;
     cin >> t;
+    while (t--) {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
 
-    while (t--)
-    {
-        ll n;
-        cin >> n;
-
-        string s;
-        cin >> s;
-
-        ll max_s = 0;
-
-        for (auto &it : s)
-        {
-            if (it == '1')
-                max_s++;
+    vector<bool> blocked(n, false);
+    int initial_students = 0;
+    for (int i = 0; i < n; i++) {
+        if (s[i] == '1') {
+            initial_students++;
+            blocked[i] = true;
+            if (i > 0) blocked[i - 1] = true;
+            if (i < n - 1) blocked[i + 1] = true;
         }
-
-        for (int i = 0; i + 2 < n; i++)
-        {
-            if (s[i] == '0' && s[i+1] == '0' && s[i+2] == '0')
-                max_s++;
-        }
-
-        cout << max_s << endl;
     }
+
+    int extra_students = 0;
+    int current_gap = 0;
+    for (int i = 0; i < n; i++) {
+        if (!blocked[i]) {
+            current_gap++;
+        } else {
+          
+            if (current_gap > 0) {
+                extra_students += (current_gap + 2) / 3;
+            }
+            current_gap = 0;
+        }
+    }
+
+    if (current_gap > 0) {
+        extra_students += (current_gap + 2) / 3;
+    }
+
+    cout << initial_students + extra_students << endl;
+    }
+    return 0;
 }
